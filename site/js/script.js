46895c204ce7e1af8f054c49fbc4f2e848dcf361
Aluno <ramosmatheusc@gmail.com>
2019-02-14 21:18:04 -0200

    Angular

---
+++ site/js/script.js
@@ -0,0 +1,12 @@
+function calcular() {
+  var n1 = parseInt(document.getElementById('n1').value);
+  var n2 = parseInt(document.getElementById('n2').value);
+
+  alert(n1+n2);
+}
+function fodeo(){
+  for (var i = 0; i < 15; i++) {
+    alert("O empreendedorismo talvez seja o assunto que reúne o maior número de chatos por páragrafo. Desde sempre administração e auto-ajuda caminham juntos, com fronteiras bem borradas. Mas quando se fala em empreender a coisa desanda de vez.");
+  }
+
+}
