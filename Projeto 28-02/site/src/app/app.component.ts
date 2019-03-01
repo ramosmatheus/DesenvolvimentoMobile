18edc2e1dec98b33d7bf31963640258bdc933a94
Aluno <ramosmatheusc@gmail.com>
2019-02-28 22:34:58 -0300

    TypeScript começo

--- Projeto 28-02/site/src/app/app.component.ts
+++ Projeto 28-02/site/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent{
+  nome : string = "Matheus";
+  valor1: number;
+  valor2: number;
+  total: number;
+
+  clique(): void {
+    this.total = this.somar();
+
+  }
+
+  somar(): number {
+    this.total = Number(this.valor1) + Number(this.valor2);
+
+    return this.total;
+  }
+
 }
